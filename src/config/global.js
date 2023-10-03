a86307a5d700d9814113d33b142e517c81c1a32c
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2023-10-02 22:48:38 -0500

    Accesibilidad

--- src/config/global.js
+++ src/config/global.js
@@ -165,7 +165,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/228133_CF03_DU.pdf',
       },
       {
         icono: 'fas fa-download',
